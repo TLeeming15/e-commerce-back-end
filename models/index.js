184723af7ee3c614f677e0251447c8c1ed3bf119
TLeeming15 <tyler.leeming21@gmail.com>
2023-04-02 11:25:10 +0930

    new routes

--- models/index.js
+++ models/index.js
@@ -3,14 +3,21 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { getEnabledCategories } = require('trace_events');
 
 // Products belongsTo Category
+Product.belongsTo (Category);
 
 // Categories have many Products
+Category.hasMany (Product, {foreignKey:"category_id",onDelete: 'CASCADE',
+onUpdate: 'CASCADE'}) 
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany (Tag, {through:"ProductTag"})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, { through:"ProductTag"})
+
 
 module.exports = {
   Product,
